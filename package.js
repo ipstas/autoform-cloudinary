b1b8d8f278ae6b67e1052dd8ae4230ae4a1e37a3
stan podolski <stanp@xlazz.com>
2019-05-06 15:11:40 -0500

    upd: added npm jquery

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
 	name: 'ipstas:autoform-cloudinary3',
-	version: '1.0.15',
+	version: '1.0.18',
 	summary: 'Provides a autoform augmented field for cloudinary upload',
 	git: 'https://github.com/ipstas/autoform-cloudinary',
 	documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('1.6');
+	api.versionsFrom('1.8');
 	
 	Npm.depends({
 		'cloudinary': '1.14.0',
-		//'jquery-ui': '1.12.1',
+		'jquery-ui': '1.12.1',
 		//'blueimp-file-upload': '9.28.0',
 		//'cloudinary-jquery-file-upload': '2.6.2',
 		//'blueimp-load-image': '2.20.1',
@@ -32,18 +32,18 @@ Package.onUse(function (api) {
 	api.mainModule('autoform_cloudinary_srv.js', 'server', {lazy:false});
 
 	api.addFiles([
-		'jQuery-File-Upload/jquery.ui.widget.js',
-		'jQuery-File-Upload/jquery.iframe-transport.js',
-		'jQuery-File-Upload/jquery.fileupload.js',
+		'vendor/jquery.ui.widget.js',
+		'vendor/jquery.iframe-transport.js',
+		'vendor/jquery.fileupload.js',
 		'autoform_cloudinary.css'
 	], 'client');
 	
 	//api.export(['AutoformCloudinary']);
 });
 
-Package.onTest(function(api) {
+/* Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('ipstas:autoform-cloudinary3');
   api.mainModule('autoform_cloudinary-tests.js');
-});
+}); */
